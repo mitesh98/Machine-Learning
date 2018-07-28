85bea7fe52d3ae85c3e76b708660269a4f9d9799
MITESH KUMAR <iitnmitesh@gmail.com>
2018-07-28 06:24:38 +0530

    Update twitter_sentiment_challenge.py

--- twitter_sentiment_challenge.py
+++ twitter_sentiment_challenge.py
@@ -3,11 +3,11 @@ from textblob import TextBlob
 import csv
 
 #twitter api
-consumer_key="qEcmdmB1ljpmn9prtTySPujDv"
-consumer_secret="oK9XGArKtalGJGdhckNWABu47iW9LRJXUqxKHlaWnXByXyiFBb"
+consumer_key="********************"
+consumer_secret="***************************************"
 
-access_token="847439552339890177-fxniAFXcNqe8aFKL7dRXq1AjWQeLVYJ"
-access_token_secrete="vaNSO9R0UwPadqRxQ5whaF4lVykRHaYQGsqjOvVsL7Jci"
+access_token="******************************"
+access_token_secrete="**********************"
 
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secrete)
