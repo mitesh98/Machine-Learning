58bb34b58816088904777966a3f610a6d21e769d
MITESH KUMAR <iitnmitesh@gmail.com>
2018-10-26 23:56:12 +0530

    Update categorical_data.py

--- categorical_data.py
+++ categorical_data.py
@@ -9,7 +9,6 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
-
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
